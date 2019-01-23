9ccf09f93b8273452c42192574a31fa7aed37d99
Kevin Griffin <coemgen@hotmail.com>
2019-01-23 14:27:50 -0500

    Update !convert.bat

--- !convert.bat
+++ !convert.bat
@@ -1,8 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
-for %%f in (*.mid) do (
+for %%f in (*.mxl) do (
   echo %%f
   echo %%~nf
-  "C:\Program Files (x86)\MuseScore 2\bin\musescore.exe" "%%f" -o "converted\%%~nf.mp3"
-  rem "C:\Program Files (x86)\MuseScore 2\bin\musescore.exe" "%%f" -o "%%~nf.png"
+  "C:\Program Files\MuseScore 3\bin\musescore3.exe" "%%f" -o "converted\%%~nf.mp3"
+rem  "C:\Program Files (x86)\MuseScore 2\bin\musescore.exe" "%%f" -o "%%~nf.png"
+rem  "C:\Program Files (x86)\MuseScore 2\bin\musescore.exe" "%%f" -o "%%~nf.xml"
 )
